e3937922dbad4c4035595f50e92fc630dfd65dd0
Bogdan <bogdan.lejn@mail.ru>
2022-04-17 15:28:22 +0300

    second commit

---
+++ 2sem/lab 9/lab8/lab8.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+struct Student {
+	string surname;
+	string date;
+	string group;
+	int phys;
+	int math;
+	int inf;
+	int chem;
+	double avr;
+};
+
+
+int main()
+{
+	int n;
+	Student arr[100];
+	cout << "Enter number of students:" << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "Enter student's surname and initials:" << endl;
+		cin >> arr[i].surname;
+		cout << "Enter student's date of birth:" << endl;
+		cin >> arr[i].date;
+		cout << "Enter student's group:" << endl;
+		cin >> arr[i].group;
+		cout << "Enter student's physics mark:" << endl;
+		cin >> arr[i].phys;
+		cout << "Enter student's math mark:" << endl;
+		cin >> arr[i].math;
+		cout << "Enter student's informatics mark:" << endl;
+		cin >> arr[i].inf;
+		cout << "Enter student's chem mark:" << endl;
+		cin >> arr[i].chem;
+		arr[i].avr = (arr[i].phys + arr[i].math + arr[i].inf + arr[i].chem) / 4.0;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i].inf>=4 && arr[i].math>=4) {
+			cout << arr[i].surname << " " << arr[i].date << " group " << arr[i].group <<
+				"  phys mark: " << arr[i].phys << ", math mark: " << arr[i].math << ", informatics mark: " << arr[i].inf <<
+				", chemistry mark: " << arr[i].chem << ", average: " << arr[i].avr;
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ 2sem/lab3/lab3.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+double simple(double x0, double x, double a, double eps) {
+    do
+    {
+        x = x0;
+        x0 = 0.5 * (x0 + a / x0);
+    } while (fabs(x - x0) > eps);
+    return x;
+}
+
+double recursion(double x0, double x, double a, double eps) {
+    if (fabs(x - x0) < eps)
+    {
+        return x0;
+    }
+
+    x = x0;
+    x0 = 0.5 * (x0 + a / x0);
+
+    return recursion(x0,x,a,eps);
+
+}
+
+int main()
+{   
+    double a, x, eps = 0.000000001;
+
+    cout << "Input a: ";
+    cin >> a;
+
+    double x0 = 0.5 * (1 + a);
+    x = 0;
+
+    cout << "Simple X = " << simple(x0, x, a, eps) << endl;
+    cout << "Recursion X = " << recursion(x0, x, a, eps);
+
+    return 0;
+}
